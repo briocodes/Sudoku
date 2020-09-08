8379904fdee996c3a7586fca37628884a95be9c0
Briocodes <briocodes@gmail.com>
2020-09-08 16:39:15 +0100

    initial commit

--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -19,11 +19,7 @@ public class Coordinates {
         return y;
     }
 
-    /**
-     * The equals method comes from the object class in Java, for which every class extends from the object implicitly. The essence is to be able to store this Coordinate object in a HashMap. We are going to be using them as keys to keep track of the inline elements.
-     * @param o
-     * @return
-     */
+
     @Override
     public boolean equals(Object o){
         if (this==o) return true;
@@ -32,9 +28,7 @@ public class Coordinates {
         return x == that.x &&
                 y == that.y;
     }
-    /**
-     * Implementing a hashCode function. (A hashCode is a unique identifier generated from some sort of data that is given). The Objects.hash(), generates a unique identifier for the x and y values
-     */
+
     @Override
     public int hashCode(){
         return Objects.hash(x,y);
