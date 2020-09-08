eab5793872f199119b84d506f334aad56dcb12fe
Briocodes <briocodes@gmail.com>
2020-09-08 16:33:08 +0100

    initial commit

--- src/sudoku/computationlogic/SudokuUtilities.java
+++ src/sudoku/computationlogic/SudokuUtilities.java
@@ -3,7 +3,6 @@ package sudoku.computationlogic;
 import sudoku.problemdomain.SudokuGame;
 
 public class SudokuUtilities {
-    //The utility class has a utility/static function which would copy the values from one 2-dimensional sudoku array that's passed in, into another 2-dimensional array. And it also has a second function which will take in an old array and return a new array
     public static void copySudokuArrayValues(int[][] oldArray, int[][] newArray){
         for (int xIndex=0; xIndex< SudokuGame.GRID_BOUNDARY; xIndex++){
             for (int yIndex=0; yIndex< SudokuGame.GRID_BOUNDARY; yIndex++){
